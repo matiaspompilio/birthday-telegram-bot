5ea7d4f962be8a4faba827040302d6d036b2f991
Matias <matiaspompilio97@gmail.com>
2021-04-25 01:24:53 -0300

    testing github actions

---
+++ reminder.js
@@ -0,0 +1,29 @@
+require('../config/mongo');
+const moment = require('moment-timezone');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { bot } = require('../config/api');
+const { getChats } = require('../services/chat');
+
+const checkBirthdays = async (func) => {
+  const chats = await getChats();
+  chats.map(({ chatId, users }) => {
+    users.map((user) => {
+      if ((moment().isSame(moment(user.birthday, 'DD-MM'), 'month')) && (moment().isSame(moment(user.birthday, 'DD-MM'), 'day'))) {
+        func(chatId, user.name);
+      }
+    });
+  });
+};
+
+try {
+  checkBirthdays((chatId, name) => bot.sendMessage(chatId, `Es el cumpleaños de ${name}!`));
+  const time = (new Date()).toTimeString();
+  core.setOutput('time', time);
+
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
+}
