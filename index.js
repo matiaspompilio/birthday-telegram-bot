1f1b3869ec2b4cd0db22a70af956b524ef516925
Matias <matiaspompilio97@gmail.com>
2021-04-25 18:01:44 -0300

    changin port for heroku deploy

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ require('dotenv').config();
 require('./src/config/mongo');
 const express = require('express');
 const bot = require('./src/bot');
-const { PORT } = require('./src/config/constants');
 
 const app = express();
 
 app.use(express.json());
 
-app.listen(PORT || 5000);
+app.listen(process.env.PORT || 5000);
 
 app.post(`/${bot.token}`, (req, res) => {
   bot.processUpdate(req.body);
